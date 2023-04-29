c3d4bdeb9e3076ba92b076569cbb31ce70e76de9
vineeth <119692597+vineeth38@users.noreply.github.com>
2023-04-29 22:35:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const container = document.getElementById('container');
+    const level = getLevel(container);
+    alert(`The level of the element is: ${level}`);
+
+    function getLevel(element) {
+      let level = 0;
+      while (element.parentElement) {
+        level++;
+        element = element.parentElement;
+      }
+      return level;
+    }
